34f52589a3504bf824f8277de99dfa92d46fe8b6
szymonides12 <szymon.bernal@gmail.com>
2017-11-07 12:44:13 +0100

    Add files via upload

---
+++ lab5/tests/RectangularMapTest.java
@@ -0,0 +1,69 @@
+import agh.cs.lab.Car;
+import agh.cs.lab.Position;
+import agh.cs.lab.RectangularMap;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.TestCase.assertFalse;
+
+/**
+ * Created by Student11 on 2017-11-07.
+ */
+public class RectangularMapTest {
+    @Test
+    public void canMoveTo_position_occupied_by_another_car() {
+        RectangularMap rectangularMap = new RectangularMap(10,10);
+        rectangularMap.place(new Car(rectangularMap, 4,3));
+        Car car = new Car(rectangularMap, 4,2);
+
+        rectangularMap.place(car);
+
+        assertFalse(rectangularMap.canMoveTo(new Position(4, 2)));
+        assertFalse(rectangularMap.canMoveTo(new Position(4,3)));
+
+    }
+
+    @Test
+    public void place_on_occupied_position() {
+        RectangularMap rectangularMap = new RectangularMap(10,10);
+        rectangularMap.place(new Car(rectangularMap, 4,3));
+
+        assertFalse(rectangularMap.place(new Car(rectangularMap, 4, 3)));
+        assertFalse(rectangularMap.place(new Car(rectangularMap,-20, -20)));
+    }
+
+    @Test
+    public void place_on_free_position() {
+        RectangularMap rectangularMap = new RectangularMap(10,10);
+        rectangularMap.place(new Car(rectangularMap, 4,3));
+
+        assertTrue(rectangularMap.place(new Car(rectangularMap, 4,2)));
+        assertTrue(rectangularMap.place(new Car(rectangularMap, 4,1)));
+    }
+
+    @Test
+    public void objectAt_free_position() {
+        RectangularMap rectangularMap = new RectangularMap(10,10);
+        rectangularMap.place(new Car(rectangularMap, 4,3));
+
+        assertEquals(null, rectangularMap.objectAt(new Position(4,1)));
+    }
+
+    @Test
+    public void objectAt_occupied_position() {
+        RectangularMap rectangularMap = new RectangularMap(10,10);
+        rectangularMap.place(new Car(rectangularMap, 4,3));
+
+        assertNotNull(rectangularMap.objectAt(new Position(4,3)));
+
+    }
+
+    @Test
+    public void run() {
+
+    }
+
+}
